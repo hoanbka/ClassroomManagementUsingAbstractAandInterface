a798295d43bd8f7dec74c517fb96bc14ba7f2f73
Albert <nguyenhoan.hut@gmail.com>
2016-07-23 09:31:12 +0700

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,10 +1,10 @@
 package student;
 
 public abstract class Person {
-	public String name;
-	public int age;
+	protected String name;
+	protected int age;
 
-	public Person(String name, int age) {
+	protected Person(String name, int age) {
 		this.name = name;
 		this.age = age;
 	}
